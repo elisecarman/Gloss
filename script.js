5a7f448d9920d35020ad363cf13d9e0919b45a12
elisecarman <77816390+elisecarman@users.noreply.github.com>
2022-09-04 00:03:32 -0700

    database strucutre is now branch like. submit is connected to databse, and so is script. One bug currently being fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //START: ACCORDION
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -19,35 +20,89 @@ for (i = 0; i < acc.length; i++) {
 // INSERT ON HOVER POP UPS IN PAGE
 replaceText(document.body)
 
-function replaceText(element) {
+async function replaceText(element) {
 
-  const dict = {"Murphy" : "Muwwy",
-              "actor" : "acwor"}
+  domainPieces = await getActiveTabURL()
+  
+  
+  
+  /* domainPieces = ["en.wikipedia.org","wiki","Cillian_Murphy"] */
+  
+  dict = await fetchDatabase(domainPieces[0])
+  currDict = dict
+
+  for (p in domainPieces){
+    if (p > 0){
+      if (domainPieces[p] in currDict){
+        currDict = currDict[domainPieces[p]]
+      } else{
+        break;
+      }
+    }
+  }
+  
+
+  var acronyms = {}
+  var regexString = ''
+
+  if ("acronyms" in currDict){
+   
+    acronyms = currDict["acronyms"]
+    regexString = currDict["regex"]
+  }
+
+  /* console.log(acronyms)
+  console.log(regexString)
+   */
+  const regex = new RegExp(regexString, "g")
+
+  if (Object.keys(acronyms).length != 0){
 
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText)
   } else if (element.nodeType === Text.TEXT_NODE) {
-    if ( p = element.textContent.match(/(?<![A-Z])Murphy(?![A-Z])/g)) {
+    if ( p = element.textContent.match(regex)) {
       const newElement = document.createElement('div')
      
-      console.log(p)
-
+      console.log("all the way here")
       if (p.length > 1){
         
         for(i in p) {
-          const regex = new RegExp(`(?<![A-Z])${p[i]}(?![A-Z])`, "g");
-          const replacement = '<div class="tooltip">' + p[i] + '<span class="tooltiptext">' + dict[p[i]] + '</span> </div>'
-          newElement.innerHTML = element.textContent.replace(regex, replacement)
+          const partialRegex = new RegExp(`(?<![A-Z])${p[i]}(?![A-Z])`, "g");
+          const replacement = '<div class="tooltip">' + p[i] + '<span class="tooltiptext">' + acronyms[p[i]] + '</span> </div>'
+          newElement.innerHTML = element.textContent.replace(partialRegex, replacement)
           element.replaceWith(newElement) 
         }
 
     } else {
-      const replacement = '<div class="tooltip"> $1 <span class="tooltiptext">' + dict[p] + '</span> </div>'
-      newElement.innerHTML = element.textContent.replace(/((?<![A-Z])Murphy(?![A-Z]))/gi, replacement)
-        element.replaceWith(newElement)
+      const replacement = '<div class="tooltip">' + p + '<span class="tooltiptext">' + acronyms[p] + '</span> </div>'
+      newElement.innerHTML = element.textContent.replace(regex, replacement)
+      element.replaceWith(newElement)
     }
     }
-  }
+  }}
 }
 
 // test page: https://en.wikipedia.org/wiki/Cillian_Murphy
+
+function fetchDatabase(domain){
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([domain], (obj) => {
+      resolve(obj[domain] ? (obj[domain]) : []);
+    });
+  });
+}
+
+async function getActiveTabURL() {
+  const tabs = await chrome.tabs.query({
+      currentWindow: true,
+      active: true
+  });
+  var fullURL = tabs[0].url
+  var pieces = fullURL.split("/")
+
+  pieces.shift()
+  pieces.shift()
+
+  return fullURL;
+}
--- submit.js
+++ submit.js
@@ -6,12 +6,70 @@ submitButton.addEventListener("submit", (e) => {
   e.target.elements.json.value = "";
 });
 
-function submit(data) {
+async function submit(data) {
   try {
-    parsed = JSON.parse(data);
+
+    /* 
+    {"items":[]}
+    */
+    const parsed = JSON.parse(data);
+    domainPieces = await getActiveTabURL()
+    console.log(domainPieces)
+
+    parseFile(domainPieces, parsed)
+
   } catch (error) {
     console.log(error);
   }
-  console.log(parsed);
-  return parsed;
+
+ 
 }
+
+async function parseFile(domainPieces, data) {
+  //assuming the format is the following:
+  // {items : [data]}
+  const items = data["items"]
+
+  //create regex expression
+  const acronyms = Object.keys(items)
+  var regex = ``
+
+  for (a in acronyms){
+    if (a < acronyms.length - 1){
+       regex += `(?<![A-Z])${acronyms[a]}(?![A-Z])|`
+    } else{
+      regex += `(?<![A-Z])${acronyms[a]}(?![A-Z])`
+    }
+  }
+
+  //create dict structure for URL
+  var insert = {}
+  var currentLevel = insert
+  for (p in domainPieces){
+    if (p > 0){
+      currentLevel[domainPieces[p]] = {}
+      currentLevel = currentLevel[domainPieces[p]]
+    } 
+  }
+
+  currentLevel["acronyms"] = items
+  currentLevel["regex"] = regex
+
+  
+  chrome.storage.sync.set({
+    [domainPieces[0]]: insert
+  });
+  
+  result = await fetchDatabase(domainPieces[0])
+  console.log(result)
+}
+
+
+function fetchDatabase(domain){
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([domain], (obj) => {
+      resolve(obj[domain] ? (obj[domain]) : []);
+    });
+  });
+}
+
