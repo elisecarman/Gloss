c2d7ed9d63a1b32eaea178aadc22c8cabd678316
Jazem Siddiqui <jazemf.s@gmail.com>
2022-09-03 18:18:04 -0700

    basic ui with working submit

--- submit.js
+++ submit.js
@@ -1,9 +1,17 @@
 submitButton = document.getElementById("json-input");
+
 submitButton.addEventListener("submit", (e) => {
   e.preventDefault();
   submit(e.target.elements.json.value);
+  e.target.elements.json.value = "";
 });
 
 function submit(data) {
-  console.log(data);
+  try {
+    parsed = JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(parsed);
+  return parsed;
 }
