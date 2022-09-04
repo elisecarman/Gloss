d0cc9748eea3891efbe048d355f56ebfd8319768
Jazem Siddiqui <jazemf.s@gmail.com>
2022-09-03 18:29:41 -0700

    fix accordion

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-replaceText(document.body)
+//START: ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+//END: ACCORDION
+
+replaceText(document.body);
 
 function replaceText(element) {
   if (element.hasChildNodes()) {
-    element.childNodes.forEach(replaceText)
+    element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
     if (element.textContent.match(/Murphy/gi)) {
-      const newElement = document.createElement('div')
-      newElement.innerHTML = element.textContent.replace(/(Murphy)/gi, '<div class="tooltip"> $1 <span class="tooltiptext">Tooltip text</span> </div>')
-      element.replaceWith(newElement)
+      const newElement = document.createElement("div");
+      newElement.innerHTML = element.textContent.replace(
+        /(Murphy)/gi,
+        '<div class="tooltip"> $1 <span class="tooltiptext">Tooltip text</span> </div>'
+      );
+      element.replaceWith(newElement);
     }
   }
 }
