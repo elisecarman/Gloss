f1425a04e04dccc5a0e3b97638dfd7a33f552d95
Jazem Siddiqui <jazemf.s@gmail.com>
2022-09-28 09:42:19 -0700

    latest

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //START: ACCORDION
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -16,93 +15,96 @@ for (i = 0; i < acc.length; i++) {
 }
 //END: ACCORDION
 
-
 // INSERT ON HOVER POP UPS IN PAGE
-replaceText(document.body)
+replaceText(document.body);
 
 async function replaceText(element) {
+  domainPieces = await getActiveTabURL();
+  console.log(domainPieces[0]);
 
-  domainPieces = await getActiveTabURL()
-  
-  
-  
   /* domainPieces = ["en.wikipedia.org","wiki","Cillian_Murphy"] */
-  
-  dict = await fetchDatabase(domainPieces[0])
-  currDict = dict
 
-  for (p in domainPieces){
-    if (p > 0){
-      if (domainPieces[p] in currDict){
-        currDict = currDict[domainPieces[p]]
-      } else{
+  dict = await fetchDatabase(domainPieces[0]);
+  currDict = dict;
+  // console.log(currDict);
+
+  for (p in domainPieces) {
+    if (p > 0) {
+      if (domainPieces[p] in currDict) {
+        currDict = currDict[domainPieces[p]];
+      } else {
         break;
       }
     }
   }
-  
 
-  var acronyms = {}
-  var regexString = ''
+  var acronyms = {};
+  var regexString = "";
 
-  if ("acronyms" in currDict){
-   
-    acronyms = currDict["acronyms"]
-    regexString = currDict["regex"]
+  if ("acronyms" in currDict) {
+    acronyms = currDict["acronyms"];
+    regexString = currDict["regex"];
   }
 
   /* console.log(acronyms)
   console.log(regexString)
    */
-  const regex = new RegExp(regexString, "g")
+  const regex = new RegExp(regexString, "g");
 
-  if (Object.keys(acronyms).length != 0){
+  if (Object.keys(acronyms).length != 0) {
+    if (element.hasChildNodes()) {
+      element.childNodes.forEach(replaceText);
+    } else if (element.nodeType === Text.TEXT_NODE) {
+      if ((p = element.textContent.match(regex))) {
+        const newElement = document.createElement("div");
 
-  if (element.hasChildNodes()) {
-    element.childNodes.forEach(replaceText)
-  } else if (element.nodeType === Text.TEXT_NODE) {
-    if ( p = element.textContent.match(regex)) {
-      const newElement = document.createElement('div')
-     
-      console.log("all the way here")
-      if (p.length > 1){
-        
-        for(i in p) {
-          const partialRegex = new RegExp(`(?<![A-Z])${p[i]}(?![A-Z])`, "g");
-          const replacement = '<div class="tooltip">' + p[i] + '<span class="tooltiptext">' + acronyms[p[i]] + '</span> </div>'
-          newElement.innerHTML = element.textContent.replace(partialRegex, replacement)
-          element.replaceWith(newElement) 
+        console.log("all the way here");
+        if (p.length > 1) {
+          for (i in p) {
+            const partialRegex = new RegExp(`(?<![A-Z])${p[i]}(?![A-Z])`, "g");
+            const replacement =
+              '<div class="tooltip">' +
+              p[i] +
+              '<span class="tooltiptext">' +
+              acronyms[p[i]] +
+              "</span> </div>";
+            newElement.innerHTML = element.textContent.replace(partialRegex, replacement);
+            element.replaceWith(newElement);
+          }
+        } else {
+          const replacement =
+            '<div class="tooltip">' +
+            p +
+            '<span class="tooltiptext">' +
+            acronyms[p] +
+            "</span> </div>";
+          newElement.innerHTML = element.textContent.replace(regex, replacement);
+          element.replaceWith(newElement);
         }
-
-    } else {
-      const replacement = '<div class="tooltip">' + p + '<span class="tooltiptext">' + acronyms[p] + '</span> </div>'
-      newElement.innerHTML = element.textContent.replace(regex, replacement)
-      element.replaceWith(newElement)
+      }
     }
-    }
-  }}
+  }
 }
 
 // test page: https://en.wikipedia.org/wiki/Cillian_Murphy
 
-function fetchDatabase(domain){
+function fetchDatabase(domain) {
   return new Promise((resolve) => {
     chrome.storage.sync.get([domain], (obj) => {
-      resolve(obj[domain] ? (obj[domain]) : []);
+      resolve(obj[domain] ? obj[domain] : []);
     });
   });
 }
 
 async function getActiveTabURL() {
   const tabs = await chrome.tabs.query({
-      currentWindow: true,
-      active: true
+    currentWindow: true,
+    active: true,
   });
-  var fullURL = tabs[0].url
-  var pieces = fullURL.split("/")
-
-  pieces.shift()
-  pieces.shift()
-
-  return fullURL;
+  var fullURL = tabs[0].url;
+  var pieces = fullURL.split("/");
+  pieces.shift();
+  pieces.shift();
+  console.log(pieces);
+  return pieces;
 }
